0c1a29087e17013dbba01662abac21d0dfbfe589
Iryna <yeremizina1@gmail.com>
2026-03-03 08:52:18 +0000

    add components counter and random

--- reactbase/src/Counter.js
+++ reactbase/src/Counter.js
@@ -1,19 +1,60 @@
-import React, {Fragment, useState} from 'react';
+// import React, {Fragment, useState} from 'react';
+
+// const CounterView = ({counterValue, onIncrement}) => {
+//     <Fragment>
+//         <p>{counterValue}</p>
+//         <button type="button" onClick={onIncrement}>
+//             Increment
+//         </button>
+//     </Fragment>
+// }
+
+// const Counter = () => {
+//     const [counter, setCounter] = useState(0);
+
+//     const onIncrementHandler = () => {
+//         setCounter(counter + 1);
+//     };
+
+//     return (
+//         // <Fragment>
+//         //     <p>{counter}</p>
+//         //     <button type="button" onClick={onClickHandler}>
+//         //         Increment
+//         //     </button>
+//         // </Fragment>
+//         <CounterView
+//          counterValue={counter} 
+//          onIncrement={onIncrementHandler}
+//         />
+//     );
+// };
+
+// export default Counter;
+//
+//
+import React, {useState} from 'react';
+import InteractiveView from './InteractiveView';
+
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
 
-    const onClickHandler = () => {
+    const onIncrementHandler = () => {
         setCounter(counter + 1);
     };
 
     return (
-        <Fragment>
-            <p>{counter}</p>
-            <button type="button" onClick={onClickHandler}>
-                Increment
-            </button>
-        </Fragment>
+        // <Fragment>
+        //     <p>{counter}</p>
+        //     <button type="button" onClick={onClickHandler}>
+        //         Increment
+        //     </button>
+        // </Fragment>
+        <CounterView
+         counterValue={counter} 
+         onIncrement={onIncrementHandler}
+        />
     );
 };
 
---
+++ reactbase/src/Random.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import InteractiveView from './InteractiveView';
+
+// const Random = () => {
+//     const [randomNumber, setRandomNumber] = useState(0);
+
+//     return <InteractiveView
+//         value={randomNumber}
+// };
+
+export default Random;
